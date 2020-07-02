c0af45097a79313dc4df2ec6770cfbe926a5edd0
Michael Chen <45020248+fl4mx@users.noreply.github.com>
2020-07-02 15:22:09 +1000

    Update blockdelcore.cs
    Added some comments for readability

--- blockdelcore.cs
+++ blockdelcore.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
  
 public class blockdelcore : MonoBehaviour
@@ -7,42 +7,30 @@ public class blockdelcore : MonoBehaviour
  
   void OnMouseOver()
   {
-  //LMB down
+  // LMB down
   if (Input.GetMouseButtonDown(0))
   {
-    if (Time.timeScale == 1f){ //to prevent loophole where you can pause game and continue building.
+    if (Time.timeScale == 1f){ //to prevent loophole where you can pause game and continue building, to rescue yourself
       // Destroy the parent of the face we clicked
       Destroy(this.transform.parent.gameObject);
     }
   }
  
-  //RMB down
+  // RMB down
   if (Input.GetMouseButtonDown(1))
   {
-    //Added code to fix block glitches (building into player and then getting stuck)
+    // Added code to fix block glitches (building into player and then getting stuck)
     if (Time.timeScale == 1f){
       Vector3 newPosition = this.transform.parent.transform.position + delta;
-      
-
-      /*GameObject FPSController = (GameObject)GameObject.Find("FPSController");
-      float playerX = Mathf.Abs(FPSController.transform.position.x);
-      float playerY = Mathf.Abs(FPSController.transform.position.y);
-      float playerZ = Mathf.Abs(FPSController.transform.position.z);*/
-
-      //if((Mathf.Abs(newPosition.x) < (playerX - 0.45)) && (Mathf.Abs(newPosition.x) > (playerX + 0.45)) && (Mathf.Abs(newPosition.z) < (playerZ - 0.45)) && (Mathf.Abs(newPosition.z) > (playerZ + 0.45)) && (Mathf.Abs(newPosition.y) < (playerY - 0.95)) && (Mathf.Abs(newPosition.y) > (playerY + 0.95)))
-      /*if(!(((playerX + 0.4) < (newPosition.x + 0.5)) || ((playerX + 0.4) > (newPosition.x - 0.5)) || ((playerX - 0.4) < (newPosition.x + 0.5)) || ((playerX - 0.4) < (newPosition.x - 0.5)) ||  //x block check 
-          ((playerY + 0.4) < (newPosition.y + 0.5)) || ((playerY + 0.4) > (newPosition.y - 0.5)) || ((playerY - 0.4) < (newPosition.y + 0.5)) || ((playerY - 0.4) < (newPosition.y - 0.5)) ||   //y block check
-          ((playerZ + 0.4) < (newPosition.z + 0.5)) || ((playerZ + 0.4) > (newPosition.z - 0.5)) || ((playerZ - 0.4) < (newPosition.z + 0.5)) || ((playerZ - 0.4) < (newPosition.z - 0.5))))    //z block check*/
-
-
-
       if (newPosition.y < (PlayerPrefs.GetInt("worldY") + 1))
       {
-        // Call from WG script
-        WorldGenerator.BlockBuilder(newPosition, // N = C + delta
+        // Call from worldgenerator script
+        WorldGenerator.BlockBuilder(newPosition, // where newPosition = oldPosition + delta
         this.transform.parent.gameObject);
+        // Count the number of blocks placed for blocksplaced stat
         float blockplacecounter = PlayerPrefs.GetFloat("blocksplaced");
         blockplacecounter += 1;
+        // Similarly update the playerpref value
         PlayerPrefs.SetFloat("blocksplaced", blockplacecounter);
       }
     }
