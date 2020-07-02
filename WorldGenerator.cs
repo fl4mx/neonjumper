65baeb278b76a29f871a91959fd798b99edf0a6a
Michael Chen <45020248+fl4mx@users.noreply.github.com>
2020-07-02 15:06:28 +1000

    Update WorldGenerator.cs
    Fix up some comments xd

--- WorldGenerator.cs
+++ WorldGenerator.cs
@@ -31,15 +31,8 @@ public class WorldGenerator : MonoBehaviour
   private float startTime;
   private float surviveTime;
 
-  //int blocksplaced;
 
-/*
-  public GameObject obj;
-  public Vector3 objpos;
-  public String objname;
-*/
-
-  // Use this for initialization
+  //Start coroutines and variable inits
   void Start () 
   {
     // Grab playerprefs and start SimpleGenerator coroutine
@@ -59,7 +52,7 @@ public class WorldGenerator : MonoBehaviour
     SizeX = PlayerPrefs.GetInt("worldX");
     SizeZ = PlayerPrefs.GetInt("worldZ");
     SizeY = PlayerPrefs.GetInt("worldY");
-    //switch coroutine between legacy and new blockdel
+    //Toggle switches between coroutine between legacy and new blockdel
     StartCoroutine(SimpleGenerator());
     if (legacyblockdel == 0)
     {
@@ -74,13 +67,13 @@ public class WorldGenerator : MonoBehaviour
     startTime = Time.time;
   }
   
-  // Update is called once per frame
+  // Update playerPref values on each frame tick
   void Update () 
   {  	
     surviveTime = Time.time - startTime;
     PlayerPrefs.SetFloat("surviveTime", surviveTime);
-    arrayadder();
-    //Debug.Log(blockArray[1]); check to see if syntax for adding gameobjects to array works
+    arrayadder(); //add GameObjects to array
+    //Debug.Log(blockArray[1]); check to see GameObjects are indeed getting added to the array
     timesurvivedtext.GetComponent<TextMesh>().text = PlayerPrefs.GetFloat("surviveTime").ToString();
     blocksplacedtext.GetComponent<TextMesh>().text = PlayerPrefs.GetFloat("blocksplaced").ToString();
   }
@@ -91,25 +84,26 @@ public class WorldGenerator : MonoBehaviour
   }
 
 
-//new blockdeletion, 3 x y x 3 matrix below u for death optimisation
+// New blockdeletion, 3 x y x 3 matrix below player for death optimisation
   public void BlockDelete()
   {
     Destroy(blockArray[Random.Range(0, blockArray.Length)]);
   }
 
 
-//legacy blockdeletion, full rng across terrain
+// Legacy block decay algorithm, full rng across entire terrain dimensions, not the available blocks.
   public void legacyBlockDelete()
   {
     int counter = 0;
     int total = Mathf.RoundToInt(Mathf.Ceil((PlayerPrefs.GetInt("worldX") + PlayerPrefs.GetInt("worldY") + PlayerPrefs.GetInt("worldZ"))/4));
     while (counter < total)
     {
-      //Vector3 objpos = new Vector3(Mathf.Floor(Random.Range(0.0f, SizeX + 1)), Mathf.Floor(Random.Range(0.0f, SizeY)), Mathf.Floor(Random.Range(0.0f, SizeZ + 1))); keeps it in the range of pregenerated blocks, can extend to cover the built blocks.
+      //Vector3 objpos = new Vector3(Mathf.Floor(Random.Range(0.0f, SizeX + 1)), Mathf.Floor(Random.Range(0.0f, SizeY)), Mathf.Floor(Random.Range(0.0f, SizeZ + 1)));
+      //Aims to keep objpos in the range of pregenerated blocks, can extend to cover the built blocks.
       Vector3 objpos = new Vector3(Mathf.Floor(Random.Range(0.0f, SizeX + 1)), Mathf.Floor(Random.Range(0.0f, SizeY + 1)), Mathf.Floor(Random.Range(0.0f, SizeZ + 1)));
       string objname = "Cube@" + objpos;
       GameObject obj = (GameObject)GameObject.Find(objname);
-      //Debug.Log(obj);
+      //Debug.Log(obj); check if obj has the correct value.
       Destroy(obj);
       counter++;
     }
@@ -117,43 +111,43 @@ public class WorldGenerator : MonoBehaviour
 
   public static void BlockBuilder(Vector3 newPosition, GameObject originalGameobject)
   {
-    // Clone
+    // Clone and Build voxel
     GameObject clone = (GameObject)Instantiate(originalGameobject, newPosition, Quaternion.identity);
-    // Place
+    // Repositioning voxel
     clone.transform.position = newPosition;
-    // Rename
+    // Rename voxel to new position
     clone.name = "Cube@" + clone.transform.position;
   }
 
   IEnumerator SimpleGenerator()
   {
-   //this is a coroutine, generate 60 voxels per frame. 60 is a nice number. unsigned int because counting
-   uint numberOfInstances = 0;
-   uint instancesPerFrame = 120;
+    //Coroutine, generate 60 voxels per frame. 60 is a relatively good amount of GameObjects to build each frame for good FPS
+    //60 builds per frame also ensures the player to not spawn on a decayed block.
+    uint numberOfInstances = 0;
+    uint instancesPerFrame = 120;
    
-   for(int x = 1; x <= SizeX; x++) //size x is defined in the game (is a variable for modularity)
-   {
-     for(int z = 1; z <= SizeZ; z++) //so is size z
+    for(int x = 1; x <= SizeX; x++) //SizeX is worldX
+    {
+     for(int z = 1; z <= SizeZ; z++) //vice versa
      {
-       //rng a height to chuck the voxel
-      float height = Random.Range(0, SizeY); //and size y
+      //RNG terrain placements for blocks one by one
+      float height = Random.Range(0, SizeY); //similarly as before, SizeY is worldY
       for (int y = 0; y <= height; y++)
       {
-        //works out the position of the new voxel to chuck it
+        //Determine position of new block
         Vector3 newPosition = new Vector3(x, y, z);
-        //BlockBuilder is the method that builds voxels ^^^^^ up there somewhere
+        //Call blockbuilder to build at newPosition
 		    BlockBuilder(newPosition, Voxel);
-    		// + the number of times this has been done to keep count
+    		//+ the number of times this has been done to keep count
     		numberOfInstances++;
  
-        // if number of instances per frame was all good, then nothing crashed and burned,
+        //If number of instances per frame was all good
         if(numberOfInstances == instancesPerFrame)
         {
-          // Reset numberOfInstances
+          //Reset numberOfInstances
           numberOfInstances = 0;
-          // Wait for next frame
+          //Wait for next frame
           yield return new WaitForEndOfFrame();
-          //HELP IDEK WHAT IS GOING ON HERE?????????????
       }
      }
     }
@@ -161,6 +155,7 @@ public class WorldGenerator : MonoBehaviour
   }
 
 
+//Move the arena to center it around the generated island
   public void ArenaReposition()
   {
     Vector3 ArenaNewPosition = new Vector3(SizeX/2, SizeY/2, SizeZ/2);
